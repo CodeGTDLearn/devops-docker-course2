6b9f4add90c07173e05085f7e65d1b941152a789
PauloPortfolio <pvba02@gmail.com>
2020-03-26 12:34:23 -0600

    updating project

---
+++ 04-spring-boot-react-full-stack-h2/frontend/todo-app/build/precache-manifest.6bdfae21b0d9eaee221a0aacd90a8155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "facc9f7bf5d7bde359ea",
+    "url": "/static/css/main.09489565.chunk.css"
+  },
+  {
+    "revision": "facc9f7bf5d7bde359ea",
+    "url": "/static/js/main.37fd9d8d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4c4e45c21b9d5021aa11",
+    "url": "/static/js/2.aecb81c7.chunk.js"
+  },
+  {
+    "revision": "ed576ed841a4c55da9de2a5c8ec750cf",
+    "url": "/index.html"
+  }
+];
